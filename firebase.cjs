9abf985964db88f38dac30ef2ed84f54ca4c2cf4
StatBot BoostClub <statbot.boostclub@gmail.com>
2025-05-20 19:38:22 +0200

    🛠 Correction finale Firebase : admin.database fonctionnel

--- firebase.cjs
+++ firebase.cjs
@@ -5,15 +5,17 @@ const rawConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 rawConfig.private_key = rawConfig.private_key.replace(/\\n/g, '\n');
 
 // 🔧 Initialisation Firebase
-const app = admin.initializeApp({
-  credential: admin.credential.cert(rawConfig),
-  databaseURL: 'https://statbot-626ff-default-rtdb.europe-west1.firebasedatabase.app'
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(rawConfig),
+    databaseURL: 'https://statbot-626ff-default-rtdb.europe-west1.firebasedatabase.app'
+  });
+}
 
 const dbRealtime = admin.database();
 
 module.exports = {
   database: dbRealtime,
   dbRealtime,
-  app
+  app: admin.app()
 };
